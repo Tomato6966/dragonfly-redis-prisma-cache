9ba53c3897438bf0f15d0d4b8880acd76ff24b0b
Tomato6966 <chris.pre03@gmail.com>
2022-09-26 10:06:03 +0200

    first version release

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # dragonfly-redis-prisma-cache
 A Cache middleware for dragonfly (faster then redis), which still can be used with redis.
+
+# Install Package
+```
+npm i Tomato6966/dragonfly-redis-prisma-cache
+```
+
+# Use Package
+```js
+import { PrismaClient } from '@prisma/client'
+import { prismaDragonflyRedisCache } from 'prisma-cache-middleware';
+
+const prisma = new PrismaClient();
+
+prisma.$use(prismaDragonflyRedisCache({
+    redisOptions:{
+        // connection hostname/ipaddress
+        host: "localhost",
+        // Port for the dragonfly instance
+        port: 6379,
+        // your password don't provide it if not needed
+        password: "youshallnotpass",
+        // when to cancel the requests after X milli-seconds
+        timeout: 2000,
+        /** If undefined | <= 0 then no Pool will be used */
+        min_conn: 100,
+        // dragonfly can handle hundred of thousands connections at once
+        max_conn: 1000,
+        //  tls: {
+        //    key: Buffer.from("key_string"),
+        //    cert: Buffer.from("cert_string"),
+        //  };
+    },
+    toCache: [
+        {                      
+            model: 'Users',                
+            action: 'findFirst',           
+            ttl: 20,                       
+            keyPrefix: 'myCache'             
+        },
+        {                                
+            model: 'Users',
+            action: 'findMany',
+        }
+    ]
+}));
+
+export default prisma;
+```
+
+## Building and formatting
+```
+npm run build
+npm run lint
+npm run format
+```
---
+++ src/index.ts
@@ -0,0 +1,126 @@
+import { TedisPool, Tedis } from 'tedis';
+
+export interface CacheOptions {
+    storageOptions: {
+        host?: string;
+        port?: number;
+        password?: string;
+        timeout?: number;
+        /** If undefined | <= 0 then no Pool will be used */
+        min_conn?: number;
+        max_conn?: number;
+        tls?: {
+          key: Buffer;
+          cert: Buffer;
+        };
+    }
+    /** IF I should cache all Models */
+    useAllModels: boolean;
+
+    toCache: {
+        /** all models to use () */
+        model: string,
+        /** all actions to use the cache on */
+        actions: string[],
+        /** Time to live */
+        ttl?: number,
+        /** Prefix for the cache key */
+        prefix?: string
+    }[]
+}
+
+export type MiddlewareParameters = {
+    model?: string;
+    actions: string[];
+    args: any;
+    dataPath: string[];
+    runInTransaction: boolean;
+}
+
+class prismaDragonflyRedisCacheMiddleware <Prisma> {
+    private client: TedisPool | Tedis;
+    private isPool: boolean;
+    private useAllModels: boolean;
+    private toCache: {
+        model: string,
+        actions: string[],
+        ttl?: number,
+        prefix?: string
+    }[];
+
+    constructor(options: CacheOptions){
+        validate(options)
+        bind(this);
+        this.toCache = options.toCache;
+        this.useAllModels = options.useAllModels ?? false;
+        this.isPool = options.storageOptions.min_conn && options.storageOptions.min_conn >= 1
+        if(!this.isPool) {
+            delete options.storageOptions.min_conn;
+            delete options.storageOptions.max_conn;
+            this.client = new Tedis(options.storageOptions);
+        } else {
+            if(!options.storageOptions.max_conn) options.storageOptions.max_conn = options.storageOptions.min_conn + 1;
+            else if(options.storageOptions.max_conn <= options.storageOptions.min_conn) options.storageOptions.max_conn = options.storageOptions.min_conn + 1;
+            this.client = new TedisPool(options.storageOptions);
+        }
+    }
+
+    public async handle(params: MiddlewareParameters, next: (params: MiddlewareParameters) => Promise<any>){
+        let result: any = null;
+        const instance = this.toCache.find(instance => (this.useAllModels || instance.model === params.model) && instance.actions.includes(params.action))
+        if(instance){
+            const cacheKey = `${instance.prefix ? `${instance.prefix}-`: ``}${params.model}:${params.action}:${JSON.stringify(params.args)}`;
+            const tedis = this.isPool ? await this.getTedis() : this.client;
+            const findCache = await tedis.get(cacheKey);
+
+            if(findCache) {
+                result = JSON.parse(findCache);
+            }
+            else {
+                // using stringified results, because that way it uses PPC2 from dragonfly to save 54% storage space
+                result = await next(params);
+                if(instance.ttl) {
+                    await tedis.set(cacheKey, JSON.stringify(result), 'EX', instance.ttl)
+                } else {
+                    await tedis.set(cacheKey, JSON.stringify(result))
+                }
+            }
+            if(this.isPool) await this.putTedis(tedis);
+        } else console.log(`Could not find instance for ${params.model}`)
+
+        // not cached
+        if(!result) {
+            result = await next(params);
+        }
+        
+        return result;
+    }
+}
+
+const gAllProps = (object: any) => {
+	const p = new Set();
+	do {
+		for (const key of Reflect.ownKeys(object)) p.add([object, key]);
+	} while ((object = Reflect.getPrototypeOf(object)) && object !== Object.prototype);
+	return p;
+};
+
+function validate(options:CacheOptions) {
+    if(!options || typeof options !== "object") throw new SyntaxError("no valid cacheOptions provided")
+    if(typeof options.useAllModels !== "undefined" && typeof options.useAllModels !== "boolean") throw new SyntaxError("option useAllModels was not as a boolean provided");
+    if(typeof options.toCache !== "undefined" || !Array.isArray(options.toCache)) throw new SyntaxError("No option toCache was provided / option toCache is not a valid Array");
+    return true;
+}
+function bind(o: any) {
+	for (const [object, key] of gAllProps(o.constructor.prototype)) {
+		if (key === 'constructor') continue;
+        // find the traget of the property
+		const target = Reflect.getOwnPropertyDescriptor(object, key);
+		if (target && typeof target.value === 'function') o[key] = o[key].bind(o);
+	}
+	return o;
+}
+
+export function prismaDragonflyRedisCache(options: CacheOptions) {
+    return new prismaDragonflyRedisCacheMiddleware(options).handle;
+}
